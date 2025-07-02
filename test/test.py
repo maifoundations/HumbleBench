e14e07d0d856a2bc8ac79c3bd4a1ee6d2d3941d3
Bingkui Tong <tbbbk@qq.com>
2025-07-02 15:10:54 +0800

    [update] stop tracking test

--- test/test.py
+++
@@ -1,4 +0,0 @@
-
-from transformers import MllamaForConditionalGeneration, AutoProcessor
-
-model = MllamaForConditionalGeneration.from_pretrained("/mnt/data2/bingkui/model_zoo/Insight-V-Reason-LLaMA3")
